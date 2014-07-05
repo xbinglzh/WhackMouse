4841f3680167d97eedfb49168dd33bfaf79d74b2
xUanBing <xbinglzh@gmail.com>
2014-07-06 01:08:42 +0800

    打地鼠工程创建
    1.工程创建 2. 使用 cocos2dx 3.0

---
+++ WhackMouse/Classes/UiView/UiLoaddingView.cpp
@@ -0,0 +1,9 @@
+//
+//  UiLoaddingView.cpp
+//  WhackMouse
+//
+//  Created by xUanBing on 14-7-6.
+//
+//
+
+#include "UiLoaddingView.h"
---
+++ WhackMouse/Classes/UiView/UiLoaddingView.h
@@ -0,0 +1,14 @@
+//
+//  UiLoaddingView.h
+//  WhackMouse
+//
+//  Created by xUanBing on 14-7-6.
+//
+//
+
+#ifndef __WhackMouse__UiLoaddingView__
+#define __WhackMouse__UiLoaddingView__
+
+#include <iostream>
+
+#endif /* defined(__WhackMouse__UiLoaddingView__) */
